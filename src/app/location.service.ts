71860effc24cbce37ce31a14252862bf64d5870c
PavleMarkovicStudy <pavle.markovic@iu-study.org>
2023-09-28 09:39:31 -0300

    changed https

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -8,7 +8,7 @@ export class LocationService {
   // Setting the api key that I've got from the openWeatherMap.org api
   private apiKey: string = '2d188648ab210c3c27ffd214c6cb2e94';
   // the url where most calls will be made
-  private apiUrl: string = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+  private apiUrl: string = 'https://api.openweathermap.org/geo/1.0/direct?q=';
 
   constructor(private http: HttpClient) {}
 
